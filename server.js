ecf02fb9599e245b8627eefe56cf3c95c893d11b
Shaun Mbhiza <smbhiza@iqbusiness.net>
2020-03-10 15:39:49 +0200

    updates

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/hacker-new-card-ui'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/hacker-new-card-ui/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
